226a9319225cd7acc79f141868289a9b54d43b1b
gerlangiosoares <gerlangio@gmail.com>
2022-09-07 14:17:21 -0300

    ex01

---
+++ ex00.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Oi tudo bem? Tenho 6 anos e programo\n\n");
+
+    printf("Valor inteiro: %d\n", 10);
+    printf("Valor real: %f\n", 3.14159265);
+    printf("Valor real com apenas duas casas: %.2f\n", 3.14159265);
+    printf("Dado de texto um caracter: %c\n", 'a');
+    printf("Dado de texto uma String: %s\n", "testando");
+}
